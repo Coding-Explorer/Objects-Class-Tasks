dc97385ed7254a6ea5e918641d81d35e2acfff06
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-06-29 01:16:50 -0700

    Add files via upload

---
+++ P. task 05/p5.js
@@ -0,0 +1,14 @@
+var isFriend = "Jiya";
+if (isFriend === "Jiya" || isFriend === "Aliya") {
+    console.log("".concat(isFriend, " is your friend."));
+}
+else {
+    console.log("".concat(isFriend, " is not your friend."));
+}
+var isFriend1 = "Mahnoor";
+if (isFriend1 === "Jiya" || isFriend1 === "Aliya") {
+    console.log("".concat(isFriend1, " is your friend."));
+}
+else {
+    console.log("".concat(isFriend1, " is not your friend."));
+}
---
+++ P. task 05/p5.ts
@@ -0,0 +1,13 @@
+let isFriend: string = "Jiya"
+if (isFriend === "Jiya" || isFriend === "Aliya") {
+    console.log(`${isFriend} is your friend.`);
+} else {
+    console.log(`${isFriend} is not your friend.`);
+}
+
+let isFriend1: string = "Mahnoor"
+if (isFriend1 === "Jiya" || isFriend1 === "Aliya") {
+    console.log(`${isFriend1} is your friend.`);
+} else {
+    console.log(`${isFriend1} is not your friend.`);
+}
